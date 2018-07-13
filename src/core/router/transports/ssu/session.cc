d1cda146abe01651ad0ff385b4454b9d3bd282fc
anonimal <anonimal@getmonero.org>
2018-07-13 03:29:59 +0000

    SSUSession: don't return on invalid session packet length
    and remove redundant throw. We would previously continue to send the
    packet on the wire (after FillHeaderAndEncrypt) even if the given length
    was invalid!

--- src/core/router/transports/ssu/session.cc
+++ src/core/router/transports/ssu/session.cc
@@ -1347,12 +1347,6 @@ void SSUSession::FillHeaderAndEncrypt(
     std::uint8_t flag) {
   // TODO(anonimal): this try block should be handled entirely by caller
   try {
-    if (len < SSUSize::HeaderMin) {
-      LOG(error)
-        << "SSUSession:" << GetFormattedSessionInfo()
-        << "unexpected SSU packet length " << len;
-      return;
-    }
     SSUSessionPacket pkt(buf, len);
     core::RandBytes(pkt.IV(), SSUSize::IV);
     pkt.PutFlag(flag | (payload_type << 4));  // MSB is 0
@@ -1441,12 +1435,6 @@ void SSUSession::FillHeaderAndEncrypt(
     std::size_t len) {
   // TODO(anonimal): this try block should be handled entirely by caller
   try {
-    if (len < SSUSize::HeaderMin) {
-      LOG(error)
-        << "SSUSession:" << GetFormattedSessionInfo()
-        << "unexpected SSU packet length " << len;
-      return;
-    }
     SSUSessionPacket pkt(buf, len);
     kovri::core::RandBytes(pkt.IV(), SSUSize::IV);  // random iv
     m_SessionKeyEncryption.SetIV(pkt.IV());
@@ -1481,13 +1469,6 @@ void SSUSession::Decrypt(
     const std::uint8_t* aes_key,
     const bool is_session)
 {
-  if (len < SSUSize::HeaderMin)
-    {
-      throw std::length_error(
-          "SSUSession:" + GetFormattedSessionInfo() + __func__
-          + ": unexpected SSU message length " + std::to_string(len));
-    }
-
   // Parse message buffer and decrypt
   SSUSessionPacket message(buf, len);
 
@@ -1515,12 +1496,6 @@ bool SSUSession::Validate(
     std::uint8_t* buf,
     std::size_t len,
     const std::uint8_t* mac_key) {
-  if (len < SSUSize::HeaderMin) {
-    LOG(error)
-      << "SSUSession:" << GetFormattedSessionInfo()
-      << __func__ << ": unexpected SSU packet length " << len;
-    return false;
-  }
   SSUSessionPacket pkt(buf, len);
   auto encrypted = pkt.Encrypted();
   auto encrypted_len = len - (encrypted - buf);
